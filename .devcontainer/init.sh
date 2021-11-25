03948eaaeefc2acfb22cee5b3281aded79f286cc
Lars Vilhuber <lars@vilhuber.com>
2021-11-25 13:34:18 -0500

    More debugging

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,4 +1,13 @@
  #!/bin/bash
- [[ -f stata.lic ]] || echo \"${localEnv.STATA_LIC_BASE64}\" | base64 -d > stata.lic 
- docker buildx install
- echo "init done."
+ if [[ ! -f stata.lic ]] 
+ then
+    if [[ -z ${localEnv.STATA_LIC_BASE64} ]]
+    then
+        echo "No license found."
+        exit 2
+    else
+        echo "${localEnv.STATA_LIC_BASE64}" | base64 -d > stata.lic 
+    fi
+fi
+docker buildx install
+echo "init done."
